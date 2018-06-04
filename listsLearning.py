85b4232013f87a3199ff702a30742377d1a31889
anton2mihail <anton2mihail@users.noreply.github.com>
2018-06-04 19:27:32 -0400

    first

---
+++ listsLearning.py
@@ -0,0 +1,9 @@
+x = [1,4,6,8,0]
+y = [2,5,7,9,0]
+print(x.sort())#.sort() only changes the list, it does not return any values
+print(x>y)
+print(sum(y))
+print(max(y))
+print(max(x))
+#if you use the list() function it creates a new list that contains the same elements individually
+#list slices include the first element to include and the last element to exclude
