20b8bb4271fd1a122051e0c82968613ab7868dbd
anton2mihail <anton2mihail@users.noreply.github.com>
2018-06-04 19:28:38 -0400

    messing with tables

---
+++ rapidfirepulsecheck.py
@@ -0,0 +1,8 @@
+values = [1,2,3,4,5]
+#moreValues = values
+#moreValues[3] = 6
+#print(values)
+
+more = values.pop(3)
+
+print(values[1:3])
---
+++ tablesinpython.py
@@ -0,0 +1,8 @@
+rows = 5
+columns = 3
+table = []
+for i in range(rows):
+    row = [0]*columns
+    table.append(row)
+
+print(table)
